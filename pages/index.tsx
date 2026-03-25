a977c0696cbaf5709c0387a08527ecbd3ad751ba
domspad788 <domspad99@gmail.com>
2026-03-25 16:36:55 -0400

    Create main dashboard component for Jarvis application

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>Main Dashboard</h1>
+            <p>Welcome to the Jarvis application!</p>
+        </div>
+    );
+};
+
+export default Dashboard;
